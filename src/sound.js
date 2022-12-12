0aeeabb67364ab33ad5d186ba2f6399c3f793405
Anton <antontitov94@gmail.com>
2022-12-12 23:02:24 +0300

    lesson1

---
+++ src/sound.js
@@ -0,0 +1,9 @@
+import { Howl } from './howler.core.js';
+
+const soundEnd = new Howl({
+    src: ['src/mediaSound/endTimerSound.mp3'],
+    html5: true,
+    volume: 0.3,
+});
+
+soundEnd.play();
---
+++ src/switcher.js
@@ -0,0 +1,12 @@
+function switcher() {
+    let x = document.getElementById("timer");
+    let y = document.getElementById("datecalc");
+      if (x.style.display === "block" & y.style.display === "none" ) {
+          x.style.display = "none";
+          y.style.display = "block";
+          
+      } else {
+        x.style.display = "block";
+        y.style.display = "none";
+      }
+    }
